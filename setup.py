5d7ca5167c10bd98053ab1f23cd19e327d8a9595
Nikolaos Perrakis <nikperrakis@gmail.com>
2020-08-28 01:20:48 +0300

    Bump version: 0.0.0a → 0.0.1

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Iolaum/podcust',
-    version='0.0.0a',
+    version='version='0.0.1'',
     zip_safe=False,
 )
