affd4d663a8006cd138fa679b9a586faddb871b2
Nikolaos Perrakis <nikperrakis@gmail.com>
2020-08-30 21:58:39 +0300

    changing travis yml

--- podcust/__init__.py
+++ podcust/__init__.py
@@ -3,5 +3,5 @@
 # fmt: off
 __author__ = """Nikolaos Perrakis"""
 __email__ = 'nikperrakis@gmail.com'
-__version__ = '0.0.14'
+__version__ = '0.0.15'
 # fmt: on
--- setup.py
+++ setup.py
@@ -65,6 +65,6 @@ setup(
         "dev": setup_requirements + test_requirements
     },
     url='https://github.com/Iolaum/podcust',
-    version='0.0.14',
+    version='0.0.15',
     zip_safe=False,
 )
