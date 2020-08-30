b793f662aeb6abe2c4e9316106da89579b7862ef
Nikolaos Perrakis <nikperrakis@gmail.com>
2020-08-30 21:41:07 +0300

    changing travis yml

--- setup.py
+++ setup.py
@@ -65,6 +65,6 @@ setup(
         "dev": setup_requirements + test_requirements
     },
     url='https://github.com/Iolaum/podcust',
-    version='0.0.11',
+    version='0.0.13',
     zip_safe=False,
 )
